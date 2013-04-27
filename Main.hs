05ebe82516924d7bb795597afd0a1f508c75924e
Alex Reinking <alexander.reinking@yale.edu>
2013-04-26 23:55:19 -0400

    Fixed README. Got rid of GHC warnings.

--- Main.hs
+++ Main.hs
@@ -1,39 +1,34 @@
 {-# LANGUAGE Arrows, ScopedTypeVariables, FlexibleInstances #-}
 module Main where
 import FinalProject.GeneticAlgorithm
-import Euterpea.Examples.MUI
 import Control.Arrow
 import System.Random
-import Data.Maybe
-import System.IO
-import Data.List
 import Euterpea
 
 poolSize :: Int
 poolSize = 100
 
 instance Gene (Double,Double) where
-    mutate g (x,y) = (x+(randomRs (-1.0,1.0) g !! 1), y+(randomRs (-1.0,1.0) g !! 2))
-    crossover (x1,y1) (x2,y2) = (x1,y2)
-    fitness (x,y) = 1/(1+x^2+y^2)
+    mutate gen (x,y) = (x+(randomRs (-1.0,1.0) gen !! 1), y+(randomRs (-1.0,1.0) gen !! 2))
+    crossover (x1,_) (_,y2) = (x1,y2)
+    fitness (x,y) = 1/(1+x^(2 :: Integer)+y^(2 :: Integer)) -- This supresses a GHc warning. Why does this even throw a warning?
 
 runMUI :: StdGen -> [(Double,Double)] -> IO ()
-runMUI g pool = runUIEx (600,600) "Genetic Music" $
+runMUI gen pool = runUIEx (600,600) "Genetic Music" $
     proc _ -> do
-        devId <- selectOutput -< ()
         (mr, cr) <- rateSelectors -< ()
         btn <- button "Advance Generation" -< ()
         btnU <- edge -< btn
-        rec let ap' = fmap (\x -> fst $ nextGeneration mr cr ap g) btnU
-                g'  = fmap (\x -> snd $ nextGeneration mr cr ap g) btnU
-                gn' = fmap (\x -> gn+1) btnU
+        rec let ap' = fmap (\_ -> fst $ nextGeneration mr cr ap gen) btnU
+                gen'  = fmap (\_ -> snd $ nextGeneration mr cr ap gen) btnU
+                generation' = fmap (\_ -> generation+(1 :: Integer)) btnU
             ap <- hold pool -< ap'
-            g  <- hold g -< g'
-            gn <- hold 0 -< gn'
+            gen  <- hold gen -< gen'
+            generation <- hold 0 -< generation'
             best <- hold (getBest pool) -< mergeE const (fmap (\x -> betterGene (getBest x) best) ap') Nothing
         title "Overall Best" geneDisplay -< best
         title "Current Best" geneDisplay -< getBest ap
-        title "Current Generation" display -< gn
+        title "Current Generation" display -< generation
 
 betterGene :: Gene a => a -> a -> a
 betterGene g1 g2 = if fitness g1 < fitness g2 then g2 else g1
@@ -50,8 +45,9 @@ geneDisplay = leftRight $ proc gene -> do
     title "Fitness" display -< fitness gene
     returnA -< ()
 
+main :: IO ()
 main = do
-    g <- newStdGen
-    let xs = take poolSize $ randomRs (5.0,10.0) g :: [Double]
-    let ys = take poolSize $ randomRs (5.0,10.0) g :: [Double]
-    runMUI g (zip xs ys)
+    gen <- newStdGen
+    let xs = take poolSize $ randomRs (5.0,10.0) gen :: [Double]
+    let ys = take poolSize $ randomRs (5.0,10.0) gen :: [Double]
+    runMUI gen (zip xs ys)
--- README.md
+++ README.md
@@ -7,12 +7,14 @@ The idea is to genetically evolve a rules-based system that the user likes. The 
 
 My plan is to implement the following:
 
-1. Genetic Algorithm framework     **done**
+1. Genetic Algorithm framework     **revising**
 2. Grammar-based music generation  *in progress*
 3. Rule inference from MIDI sample *in progress*
-4. Genetically-evolved grammars    *in progrss*
+4. Genetically-evolved grammars    *in progress*
 
 Step one has been accomplished by creating a typeclass Gene that implements `mutate`, `crossover`, and `fitness` along with functions to advance generations (lists of Genes).
 Pains have been taken to ensure that the fitness function is called exactly once per gene per generation.
+
+It now turns out that getting user-supplied fitness is difficult in Haskell. It will require some restucturing of the GA framework.
 
 All of the music operations will be done using the excellent Euterpea library (which was developed here).
