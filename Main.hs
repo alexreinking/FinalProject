54ebcac8f4080b148542ff3e56a7b8588a8f96b1
Alex Reinking <alexander.reinking@yale.edu>
2013-04-29 02:57:48 -0400

    Added training data, pool is now initialized.

--- Main.hs
+++ Main.hs
@@ -1,17 +1,19 @@
 {-# LANGUAGE Arrows, ScopedTypeVariables, FlexibleInstances #-}
 module Main where
 import FinalProject.GeneticAlgorithm
+import Euterpea.Examples.RandomMusic
+import Euterpea.Examples.LSystems
+import Data.MarkovChain as M
 import Control.Arrow
 import System.Random
 import Euterpea
 
-poolSize :: Int
-poolSize = 10
+totalDur :: Dur
+totalDur = 4*wn -- 4 bars
 
 _mutate :: StdGen -> Gene (Music Pitch) -> Gene (Music Pitch)
 _mutate gen gene = gene { self = transpose (fst $ randomR (-1,1) gen) (self gene) }
 
---crossover picks a random 16th-note boundary and switches the two
 _crossover :: StdGen -> Gene (Music Pitch) -> Gene (Music Pitch) -> Gene (Music Pitch)
 _crossover gen g1 g2 =
     let pivots = [0.0,1/16..(min (fromRational $ dur $ self g1) (fromRational $ dur $ self g2))]
@@ -19,17 +21,64 @@ _crossover gen g1 g2 =
         part1 = takeM pivot (self g1)
         part2 = dropM pivot (self g2)
      in g1 { self = part1 :+: part2, _geneFit = const 0.0 }
+
+-- Sonata in C Major (Mozart)
+
+sonataInC :: [Music Pitch]
+sonataInC = [c 5 wn, e 5 hn, g 5 hn, b 4 dhn, c 5 en, d 5 en, c 5 hn, rest hn, 
+             a 5 wn, g 5 hn, c 6 hn, g 5 hn, f 5 en, g 5 en, e 5 en, f 5 en, e 5 hn, rest hn, 
+             a 4 qn, b 4 en, c 5 en, d 5 en, e 5 en, f 5 en, g 5 en, a 5 en,
+             g 5 en, f 5 en, e 5 en, d 5 en, c 5 en, b 4 en, a 4 en,
+             g 4 qn, a 4 en, b 4 en, c 5 en, d 5 en, e 5 en, f 5 en, g 5 en,
+             f 5 en, e 5 en, d 5 en, c 5 en, b 4 en, a 4 en, g 4 en,
+             f 4 qn, g 4 en, a 4 en, b 4 en, c 5 en, d 5 en, e 5 en, f 5 en,
+             e 5 en, d 5 en, c 5 en, b 4 en, a 4 en, g 4 en, f 4 en,
+             e 4 qn, f 4 en, g 4 en, a 4 en, b 4 en, c 5 en, d 5 en, e 5 en, 
+             d 5 en, c 5 en, b 4 en, a 4 en, g 4 en, f 4 en, e 4 en,
+             d 4 qn, e 4 en, f 4 en, g 4 en, a 4 en, b 4 en, cs 5 en,
+             d 5 en, a 4 en, b 4 en, cs 5 en, d 5 en, e 5 en, f 5 en, g 5 en,
+             a 5 en, b 5 en, c 6 en, b 5 en, a 5 en, g 5 en, f 5 en, e 5 en,
+             f 5 en, g 5 en, a 5 en, g 5 en, f 5 en, e 5 en, d 5 en, c 5 en,
+             b 4 qn, g 5 qn, e 5 qn, c 5 qn, d 5 qn, g 5 qn, e 5 qn, c 5 qn,
+             d 5 hn, g 5 hn, g 4 hn, rest hn]
+
+-- Sonatina (Clementi)
+
+sonatina :: [Music Pitch]
+sonatina = [c 5 qn, e 5 en, c 5 en, g 4 qn, g 4 qn, c 5 qn, e 5 en, c 5 en, g 4 qn, g 5 qn,
+            f 5 en, e 5 en, d 5 en, c 5 en, b 4 en, c 5 en, b 4 en, c 5 en, d 5 en, c 5 en,
+            b 4 en, a 4 en, g 4 qn, rest qn, c 5 qn, e 5 en, c 5 en, g 4 qn, g 4 qn,
+            e 5 qn, g 5 en, e 5 en, c 5 qn, e 5 en, c 5 en, d 5 en, b 4 en, c 5 en, a 4 en,
+            b 4 en, g 4 en, a 4 en, fs 4 en, g 4 en, a 4 en, b 4 en, c 5 en, d 5 en, e 5 en,
+            fs 5 en, g 5 en, a 4 qn, a 5 qn, a 5 qn, a 5 qn, b 4 en, c 5 en, d 5 en, e 5 en,
+            fs 5 en, g 5 en, a 5 en, b 5 en, c 5 qn, c 6 qn, c 6 qn, c 6 qn, d 5 en, g 5 en,
+            b 5 en, d 6 en, c 6 en, b 5 en, a 5 en, g 5 en, fs 5 en, e 5 en, g 5 en, fs 5 en,
+            a 5 en, g 5 en, fs 5 en, e 5 en, e 5 en, d 5 en, c 5 en, b 4 en, d 5 en, c 5 en,
+            b 4 en, a 4 en, g 4 hn, rest hn]
+
+-- Sonata No. 7 (Haydn)
+
+sonataNo7 :: [Music Pitch]
+sonataNo7 = [g 4 qn, g 4 hn, e 4 qn, c 5 qn, c 5 hn, b 4 qn, d 5 qn, d 5 dqn, e 5 en, f 5 qn,
+             f 5 qn, f 5 hn, e 5 qn, g 5 qn, g 5 dhn, gs 5 qn, a 5 dqn, b 5 en, d 6 en, c 6 en,
+             b 5 en, a 5 en, g 5 hn, fs 5 en, g 5 en, e 5 en, fs 5 en, g 5 dhn, g 4 qn, g 4 hn,
+             e 4 qn, c 5 qn, c 5 hn, b 4 qn, d 5 qn, d 5 dqn, e 5 en, f 5 qn, f 5 qn, f 5 hn,
+             e 5 qn, g 5 qn, g 5 dhn, gs 5 qn, a 5 dqn, b 5 en, d 6 en, c 6 en, b 5 en, a 5 en,
+             g 5 hn, fs 5 en, g 5 en, e 5 en, fs 5 en, g 5 dhn, g 5 qn]
+
+-- initialize from t2
      
 initializePool :: Int -> StdGen -> GenePool (Music Pitch)
 initializePool size gen = 
-    let genes = randomList size (36,60) gen
-     in GenePool { pool = map (\x -> Gene { self = note qn (pitch x), _geneFit = const 1.0}) (fst genes),
+    let gens = take size (iterate (fst . split) gen)
+        genes = map (M.runMulti 1 [sonataNo7,sonatina,sonataInC] 0) gens
+     in GenePool { pool = map (\x -> Gene { self = takeM totalDur (line (concat x)), _geneFit = const 1.0}) genes,
                    mr = 0.2, cr = 0.8,
                    mutate = _mutate,
                    crossover = _crossover }
 
 runMUI :: StdGen -> GenePool (Music Pitch) -> IO ()
-runMUI gen genePool = runUIEx (600,600) "Genetic Music" $
+runMUI gen genePool = runUIEx (600,700) "Genetic Music" $
     proc _ -> do
         (_mr, _cr) <- rateSelectors -< ()
         btn <- edge <<< button "Advance Generation" -< ()
@@ -38,19 +87,22 @@ runMUI gen genePool = runUIEx (600,600) "Genetic Music" $
                                       Just _ -> gp {pool = newGenes, mr = _mr, cr = _cr}
                 gp'  = fmap (\_ -> fst $ nextGeneration newPool gen) btn
                 gen' = fmap (\_ -> snd $ nextGeneration newPool gen) btn
-            gp <- hold genePool -< gp'
-            g1 <- getGene -< (pool gp !! 0)
-            g2 <- getGene -< (pool gp !! 1)
-            g3 <- getGene -< (pool gp !! 2)
-            g4 <- getGene -< (pool gp !! 3)
-            g5 <- getGene -< (pool gp !! 4)
-            g6 <- getGene -< (pool gp !! 5)
-            g7 <- getGene -< (pool gp !! 6)
-            g8 <- getGene -< (pool gp !! 7)
-            g9 <- getGene -< (pool gp !! 8)
-            g10 <- getGene -< (pool gp !! 9)
-            gen  <- hold gen -< gen'
+            gp  <- hold genePool -< gp'
+            g1  <- setFitness -< (pool gp !! 0)
+            g2  <- setFitness -< (pool gp !! 1)
+            g3  <- setFitness -< (pool gp !! 2)
+            g4  <- setFitness -< (pool gp !! 3)
+            g5  <- setFitness -< (pool gp !! 4)
+            g6  <- setFitness -< (pool gp !! 5)
+            g7  <- setFitness -< (pool gp !! 6)
+            g8  <- setFitness -< (pool gp !! 7)
+            g9  <- setFitness -< (pool gp !! 8)
+            g10 <- setFitness -< (pool gp !! 9)
+            gen <- hold gen   -< gen'
         title "Last Best" geneDisplay -< getBest newPool
+
+performGene :: Gene (Music Pitch) -> Music1
+performGene gene = toMusic1 $ tempo (4/3) (self gene)
 
 rateSelectors :: (RealFrac a, Show a) => UISF () (a,a)
 rateSelectors = leftRight $ proc _ -> do
@@ -58,12 +110,11 @@ rateSelectors = leftRight $ proc _ -> do
     _cr <- title "Crossover Rate" $ withDisplay $ hSlider (0.0,1.0) 0.8 -< ()
     returnA -< (_mr, _cr)
 
-getGene :: UISF (Gene (Music Pitch)) (Gene (Music Pitch))
-getGene = leftRight $ proc gene -> do
-    title "Gene" display -< self gene
-    fit <- title "Fitness" $ withDisplay $ hSlider (0.0,10.0) 5.0 -< ()
-    playBtn <- edge <<< button "Play" -< ()
-    midiOutB -< (0, fmap (const $ musicToMsgs False [] (toMusic1 $ self gene)) playBtn)
+setFitness :: UISF (Gene (Music Pitch)) (Gene (Music Pitch))
+setFitness = leftRight $ proc gene -> do
+    fit <- title "Fitness" $ hSlider (0.0,10.0) 5.0 -< ()
+    playBtn <- title "Actions" $ edge <<< button "Play" -< ()
+    midiOutB -< (0, fmap (const $ musicToMsgs False [] (performGene gene)) playBtn)
     returnA -< gene { _geneFit = const fit }
 
 geneDisplay :: UISF (Gene (Music Pitch)) ()
@@ -71,10 +122,10 @@ geneDisplay = leftRight $ proc gene -> do
     title "Gene" display -< gene
     title "Fitness" display -< fitness gene
     playBtn <- edge <<< button "Play" -< ()
-    midiOutB -< (0, fmap (const $ musicToMsgs False [] (toMusic1 $ self gene)) playBtn)
+    midiOutB -< (0, fmap (const $ musicToMsgs False [] (performGene gene)) playBtn)
     returnA -< ()
 
 main :: IO ()
 main = do
     gen <- newStdGen
-    runMUI gen (initializePool poolSize gen)
+    runMUI gen (initializePool 10 gen)
--- README.md
+++ README.md
@@ -8,14 +8,20 @@ The idea is to genetically evolve a rules-based system that the user likes. The 
 My plan is to implement the following:
 
 1. Genetic Algorithm framework     **complete**
-2. Markov-Chain music generation   *in progress*
+2. Markov-Chain music generation   **complete**
 3. Genetically-evolved melodies    *in progress*
+4. Save to file                    *in progress*
 
 The genetic algorithm framework is implemented by creating two data constructions:
 
 1. `GenePool a`, which contains all the basic information about the gene pool, including the list of the genes themselves as well as pool-wide mutation and crossover rates, as well as the functions implementing those operations.
 2. `Gene a`, which is an interesting construction: It contains the data that represents it along with a function that evaluates its fitness. This is to allow the GA to accept user input (in the form of a lambda) as the fitness for a gene.
 
-The program will evolve a pool of ten 8-bar melodies (initialized by a Markov Chain, trained on Bach)
+The program will evolve a pool of ten 8-bar melodies (initialized by a Markov Chain, trained on Mozart, Haydn, and Clementi)
+
+Genetically-evolved melodies are becoming a possibility, but the mutation function is very bad.
+I will create a number of different mutations and have the mutation function select between them.
+
+There currently exists a bug where the best melody isn't always actually saved. I'll have to look over how/when the pool is updated and the fitnesses are fixed.
 
 All of the music operations will be done using the excellent Euterpea library (which was developed here at Yale).
